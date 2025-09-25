5396b2d313141ff0a63d5c7676f2dce66d49331a
FrillyWave <joan.gourdon@epitech.eu>
2025-09-25 14:19:14 +0200

    COmmit

---
+++ j4.py
@@ -0,0 +1,68 @@
+#1.2
+"""
+num = int((input("Entrez un nombre entier :")))
+
+if num is 42:
+    print("This is correct")
+else :
+    pass
+
+"""
+
+#1.3
+"""
+num = int((input("Entrez un nombre entier :")))
+
+if num % 2 == 0:
+    print("pair")
+
+else :
+    print("impair")
+"""
+
+#1.4
+"""
+string = str(input("Enter a string :"))
+
+if string == "open sesame":
+    print("Access denied")
+
+elif string == "will you open, you goddamn!@&/°":
+    print("access fucking granted")
+
+else :
+    print("Access denied")
+
+"""
+
+#1.5
+"""
+num = int(input("Enter an int :"))
+
+var = ""
+if num == 42:
+    print("a")
+    var += "a"
+if num <= 21:
+    print("b")
+    var += "b"
+
+if num % 2 == 0:
+    print('c')
+    var += "c"
+    
+if num // 2 < 21:
+    print("d")
+    var += "d"
+
+
+if num % 2 == 1 and num >= 45:
+    print("e")
+    var += "e"
+
+if var == "":
+    print("f")
+"""
+
+#1.6 
+

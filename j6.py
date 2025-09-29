d72a42b941ca4efc51e221af0f43bd788bd69202
FrillyWave <joan.gourdon@epitech.eu>
2025-09-29 09:43:50 +0200

    Update j6.py

--- j6.py
+++ j6.py
@@ -15,3 +15,17 @@ print(pokemon)
 
 #1.3
 
+"""
+pokemon = {
+    "Pikachu" : "Electic",
+    "Bulbausaur": "Grass",
+    "Charmander": "Fire"
+}
+
+pokemon["Pikachu"] = ["Pichu", "Raichu"]
+
+print(pokemon)
+"""
+
+#We see that the value of pikachu is replaced by the new value
+

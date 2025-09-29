fdfde38d50ab0acb3dcde4621c17a745122d3272
FrillyWave <joan.gourdon@epitech.eu>
2025-09-29 09:41:00 +0200

    Create j6.py

---
+++ j6.py
@@ -0,0 +1,17 @@
+#1.1 / 1.2 
+
+'''
+pokemon = {
+    "Pikachu" : "Electic",
+    "Bulbausaur": "Grass",
+    "Charmander": "Fire"
+}
+
+pokemon["Blaziken"] = "Fire"
+
+print(pokemon)
+'''
+
+
+#1.3
+
